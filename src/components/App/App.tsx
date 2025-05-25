924ce694daf0e6bab26340e92c6d3a8eed5c0f3b
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2025-05-25 23:10:18 +0300

    create fetch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import css from "./App.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+import { fetchMovies } from "../../services/movieService";
 
 export default function App() {
-  return <></>;
+  const [query, setQuery] = useState("");
+  const handleSearchSubmit = (searchQuery: string) => {
+    setQuery(searchQuery);
+  };
+
+  console.log("Search submitted from App:", query);
+  useEffect(() => {
+    fetchMovies({ query })
+      .then((response) => {
+        console.log("Movies fetched:", response);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
+      });
+  }, [query]);
+  return (
+    <>
+      <SearchBar onSubmit={handleSearchSubmit} />
+    </>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.tsx";
 import "modern-normalize";
-
+import { Toaster } from "react-hot-toast";
+import SearchBar from "./components/SearchBar/SearchBar.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>
 );
